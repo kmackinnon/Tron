1386f88aa5acc69353fab5b8fa2f7feb8e766c27
Gabriel Petrella <petrella.gabriel@gmail.com>
2013-11-03 13:23:37 -0500

    added get and set and constructor methods for Player class

--- src/Gameplay/Player.java
+++ src/Gameplay/Player.java
@@ -1,28 +1,64 @@
 package Gameplay;
 
 import Database.Game;
-import String;
 import Database.User;
-import Integer;
 
 public class Player {
 
-  public Integer xPos;
-
-  public Integer yPos;
+  private int xPos;
+  private int yPos;
+  private int playerNumber; //used to distinguish between player 1 and player 2
+  private int currentDirection; //1=left, 2=up, 3=down, 4=right
 
   public String colour;
+  public int direction;
 
-  public Integer direction;
-
-    public Game myGame;
-    public Controller myController;
-    public Map myMap;
-    public User myUser;
-
-  public void moveUp() {
+  public Game myGame;
+  //public Controller myController;
+  public Input myInput;
+  public Map myMap;
+  public User myUser;
+  
+  //used to give starting coords for new game 
+  public Player(int xStart, int yStart, int num, User userName){
+      this.xPos = xStart;
+      this.yPos = yStart;
+      this.playerNumber = num;
+      this.myUser = userName;
   }
-
+  public int getX(){
+      return this.xPos;
+  }
+  public int getY(){
+      return this.yPos;
+  }
+  
+  public void setX(int x){
+      this.xPos = x;
+  }
+  
+  public void setY(int y){
+      this.yPos = y;
+  }
+  
+  public int getPlayerNumber(){
+      return this.playerNumber;
+  }
+  
+  //set 1 for player 1 (Arrow keys), 2 for player 2 (WASD)
+  public void setPlayerNumber(int number){
+      this.playerNumber = number;
+  }
+  
+  public void moveUp() {
+      //needs to be currently moving left or right
+      
+      
+//      while( ){
+//          this.Ypos += 2;
+//      }
+  }
+  
   public void moveDown() {
   }
 
@@ -33,6 +69,7 @@ public class Player {
   }
 
   public void moveCurrent() {
+      //retrieves current direction
   }
 
   public void winGame() {
@@ -42,6 +79,7 @@ public class Player {
   }
 
   public String getUsername() {
+      
   return null;
   }
 
