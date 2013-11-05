1d47c464e7096c281b3a7b266da1ec728f1fb01a
Michael Williams <draringi@draringi.net>
2013-11-05 10:39:56 -0500

    Fixed up Map syntax errors, and simplified collision logic.
    Collision logic simplified by changing if chain to boolean or statement.

--- src/Gameplay/Map.java
+++ src/Gameplay/Map.java
@@ -2,8 +2,6 @@ package Gameplay;
 
 import Database.Game;
 import UserInterface.Display;
-import Boolean;
-import Integer;
 import BitSet;
 import String;
 
@@ -17,29 +15,19 @@ public class Map {
   private Display myDisplay;
   
   public void addWall(int xPos, int yPos, String colour) {
-    map.set(getCellIndex(x,y));
+    map.set(getCellIndex(xPos,yPos));
   }
 
   private boolean outside(int x, int y){
-    if (x >= width || x < 0 || y >= height || y < 0) {
-      return true;
-    } else {
-      return false;
-    }   
+    return (x >= width || x < 0 || y >= height || y < 0);   
   }
 
   private int getCellIndex(int x, int y){
-    return y * width + x
+    return y * width + x;
   }
 
   public boolean collides(int x, int y) {
-    if (outside(x,y)){
-      return true;
-    } else if (map.get(getCellIndex(x,y))){
-      return true;
-    } else {
-      return false;
-    }
+    return (outside(x,y)||map.get(getCellIndex(x,y)));
   }
 
 }
