f7c2690d0c26a3319156d0d393f979222c5e4594
Michael Williams <draringi@draringi.net>
2013-11-06 10:20:17 -0500

    Fixed Input Class back to agreed design, and Implemented the required abstract class. TODO: implement a keybinding that extends the abstract class

--- src/Gameplay/Input.java
+++ src/Gameplay/Input.java
@@ -1,38 +1,16 @@
 package Gameplay;
 
-import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-//TODO need to define an explicit KeyEvent constructor
-public class Input extends KeyEvent {
-
-	private int key; 
-	Player player;
-	
-	public static ArrayList<Integer> inputPlayer1 = new ArrayList<Integer>(
-			Arrays.asList(VK_UP, VK_DOWN, VK_LEFT, VK_RIGHT));
-	public static ArrayList<Integer> inputPlayer2 = new ArrayList<Integer>(
-			Arrays.asList(VK_W, VK_S, VK_A, VK_D));
-
-	public void keyPressed(KeyEvent e) {
-		int clicked = e.getKeyCode(); // key the user has clicked
-
-		if (player.getPlayerNumber == 1) {
-			if (inputPlayer1.contains(clicked)){
-				key = clicked;	
-			}
-
-		} else {
-			if (inputPlayer2.contains(clicked)){
-				key = clicked;
-			}
-		}
-	}
-	
-	// returns a valid key
-	public int getKey(){
-		return key;
-	}
-
+public abstract class Input {
+    private int key;
+    private Player player;
+    
+    public int checkKey(){
+        return key;
+    }
+    
+    public boolean isKey(int key) {
+        return key == this.key;
+    }
+    
+    public abstract void command();
 }
