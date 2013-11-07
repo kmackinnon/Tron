f9671c5596864d9f07f94391af02441d89a57446
Michael Williams <draringi@draringi.net>
2013-11-06 21:02:40 -0500

    Updates to Map, and Controller
    Fixed up Header Pattern for map parsing,
    Set Input listing implementation to HashedMap

--- src/Gameplay/Map.java
+++ src/Gameplay/Map.java
@@ -2,19 +2,25 @@ package Gameplay;
 
 import Database.Game;
 import UserInterface.Display;
+
 import java.util.BitSet;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Map {
+
+  private static final Pattern headerParser = Pattern.compile("\\dx\\d");
 
   private BitSet map;
   private int width;
   private int height;
 
-  private Game myGame;
-  private Display myDisplay;
+  private final Game myGame;
+  private final Display myDisplay;
   
   public void addWall(int xPos, int yPos, String colour) {
     map.set(getCellIndex(xPos,yPos));
+    myDisplay.displayWall(xPos, yPos, colour);
   }
 
   private boolean outside(int x, int y){
@@ -29,4 +35,12 @@ public class Map {
     return (outside(x,y)||map.get(getCellIndex(x,y)));
   }
 
+  private void mapParse(String mapString, String colour){
+    Matcher m = headerParser.matcher(mapString);
+  }
+  public Map(String mapString, String colour, Game game, Display display){
+    myGame = game;
+    myDisplay = display;
+    mapParse(mapString, colour);
+  }
 }
