c7c08c02e0b58aff649ab9eddc484c5bf56783af
Michael Williams <draringi@draringi.net>
2013-11-08 13:11:17 -0500

    Added Documentation to Map Class
    Basic Documentation added for public methods of the Map Class

--- src/Gameplay/Map.java
+++ src/Gameplay/Map.java
@@ -7,6 +7,10 @@ import java.util.BitSet;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+/**
+ * Contains details concerning the current map in play, and determines if a collision has occurred.
+ * @author draringi
+ */
 public class Map {
 
   private static final Pattern headerParser = Pattern.compile("\\dx\\d");
@@ -18,6 +22,12 @@ public class Map {
   private final Game myGame;
   private final Display myDisplay;
   
+  /**
+   * Adds a wall at a given co-ordinate with a given colour.
+   * @param xPos
+   * @param yPos
+   * @param colour 
+   */
   public void addWall(int xPos, int yPos, String colour) {
     map.set(getCellIndex(xPos,yPos));
     myDisplay.displayWall(xPos, yPos, colour);
@@ -30,7 +40,12 @@ public class Map {
   private int getCellIndex(int x, int y){
     return y * width + x;
   }
-
+  /**
+   * Checks if a collision would occur at a given co-ordinate.
+   * @param x
+   * @param y
+   * @return 
+   */
   public boolean collides(int x, int y) {
     return (outside(x,y)||map.get(getCellIndex(x,y)));
   }
@@ -41,7 +56,7 @@ public class Map {
   
   /**
    * Generates Map from string 
-   * @param mapString
+   * @param mapString string formated map
    * @param colour
    * @param game
    * @param display 
@@ -52,6 +67,13 @@ public class Map {
     mapParse(mapString, colour);
   }
   
+  /**
+   * Generates blank Map of given width and height
+   * @param width
+   * @param height
+   * @param game
+   * @param display 
+   */
   public Map(int width, int height, Game game, Display display){
       myGame = game;
       myDisplay = display;
