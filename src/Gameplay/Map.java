86e85fa3e7f122674cff0d8e9f070e21584aa992
Michael Williams <draringi@draringi.net>
2013-11-10 14:15:44 -0500

    Changed Map internal from JavaFX Service to JavaFX Task, plus moved more functions into Map internal

--- src/Gameplay/Map.java
+++ src/Gameplay/Map.java
@@ -23,17 +23,12 @@ public class Map {
 
   private static final Pattern headerParser = Pattern.compile("\\dx\\d");
 
-  private MapService internal;
+  private MapTask internal;
   
   private BitSet map;
   private int width;
   private int height;
-  private int numOfPlayers;
-  private boolean running;
-  private int sleep;
 
-
-  private Vector<Player> playerList;
   private final Game myGame;
   private final Display myDisplay;
   private static Controller controller;
@@ -45,20 +40,17 @@ public class Map {
    * @param colour 
    */
   public void addWall(int xPos, int yPos, String colour){
-    internal.addWall(xPos, yPos, colour);
+    return internal.addWall(xPos, yPos, colour);
   }
 
-  private boolean outside(int x, int y){
-    return (x >= width || x < 0 || y >= height || y < 0);   
-  }
-
+  /**
+   * Sets the speed of the map to a frequency hz given in Hertz
+   * @param hz
+   */
   public void setSpeed(int hz){
-    sleep = 1000/hz;
+    return internal.setSpeed(hz);
   }
 
-  private int getCellIndex(int x, int y){
-    return y * width + x;
-  }
   /**
    * Checks if a collision would occur at a given co-ordinate.
    * @param x
@@ -66,7 +58,7 @@ public class Map {
    * @return 
    */
   public boolean collides(int x, int y) {
-    return (outside(x,y)||map.get(getCellIndex(x,y)));
+    return internal.collides(x, y);
   }
 
   private void mapParse(String mapString, String colour){
@@ -75,7 +67,7 @@ public class Map {
   
   public static Map makeDemo(Display display){
     Map map = new Map(50, 50, null, display);
-    map.addPlayer(null, "0xF00", 1);//TODO: change direction to enum
+    map.addPlayer(null, "0xF00", 1); //TODO: change direction to enum
     Player player = map.getPlayer(0);
     controller.addBinding(new MovePlayerDown(KeyEvent.VK_DOWN, player));
     controller.addBinding(new MovePlayerLeft(KeyEvent.VK_LEFT, player));
@@ -87,39 +79,11 @@ public class Map {
   }
 
   public void run(){
-    gameLoop();
-    myDisplay.gameover();
+    new Thread(internal).start();
   }
 
-  public Player getPlayer(int i){
-    return playerList.get(0);
-  }
 
-  private void gameLoop(){
-    running = true;
-    Iterator<Player> it;
-    while(running){
-      for(it = playerList.iterator(); it.hasNext();){
-        Player player = it.next();
-        player.moveCurrent();
-      }
-      for(it = playerList.iterator(); it.hasNext();){
-        Player player = it.next();
-        if(collides(player.getX(), player.getY())){
-          running = false;//TODO: add player dies code once Player has alive attribute...
-        }
-      }
-      //TODO: add checks for who is alive
-      try{
-        Thread.sleep(sleep);
-      } catch (InterruptedException ex) {
-        
-      }
-    }
-  }
-
-  public void addPlayer(User user, String colour, int direction){//TODO: change direction to enum
-    numOfPlayers++;
+  public void addPlayer(User user, String colour, int direction){ //TODO: change direction to enum
     Player player = new Player(25, 25, colour, user, this, direction);
     playerList.add(player);
   }
@@ -127,14 +91,13 @@ public class Map {
   /**
    * Generates Map from string 
    * @param mapString string formated map
-   * @param colour
+   * @param colour String containing colour of base walls in hexdecimal format
    * @param game
    * @param display 
    */
   public Map(String mapString, String colour, Game game, Display display){
     myGame = game;
     myDisplay = display;
-    
     mapParse(mapString, colour);
   }
   
@@ -160,39 +123,91 @@ public class Map {
     }
   }
 
-  private class MapService extends Service<Integer> {
+  private class MapTask extends Task<Void> {
     private BitSet map;
     private int width;
     private int height;
-    private int numOfPlayers;
     private boolean running;
     private int sleep;  
     private Vector<Player> playerList;
     private final Game myGame;
     private final Display myDisplay;
+    private final Map parent;
     
     public void addWall(int xPos, int yPos, String colour) {
       map.set(getCellIndex(xPos,yPos));
       myDisplay.displayWall(xPos, yPos, colour);
     }
+    
+    public MapTask (int width, int height, Game game, Display display, BitSet map, Map parent){
+      myGame = game;
+      myDisplay = display;
+      this.width=width;
+      this.height=height;
+      this.map = map;
+      playerList = new Vector();
+      this.parent = parent;
+    }
 
     @Override
-    protected Task<Integer> createTask(){  
-      return new Task<Integer>() {
-        protected Integer call() {
-          Integer result = null;
-          return result;
+    public Void call() {
+      running = true;
+      while(running){
+        if(isCancelled()) {
+          break;
         }
-      };
+        listenPlayers();
+        gameRound();
+      }
+      myDisplay.gameover();
+      return null;
     }
-    
-    public MapService (int width, int height, Game game, Display display, BitSet map){
-        myGame = game;
-        myDisplay = display;
-        this.width=width;
-        this.height=height;
-        this.map = map;
-        playerList = new Vector();
+
+    public void setSpeed(int hz){
+      sleep = 1000/hz;
+    }
+
+    private int getCellIndex(int x, int y){
+      return y * width + x;
+    }
+
+    private boolean outside(int x, int y){
+      return (x >= width || x < 0 || y >= height || y < 0);   
+    }
+
+    public Player getPlayer(int i){
+      return playerList.get(0);
+    }
+
+    private void gameRound(){
+      Iterator<Player> it;
+      for(it = playerList.iterator(); it.hasNext();){
+        Player player = it.next();
+        player.moveCurrent();
+      }
+      for(it = playerList.iterator(); it.hasNext();){
+        Player player = it.next();
+        if(collides(player.getX(), player.getY())){
+          running = false;//TODO: add player dies code once Player has alive attribute...
+        }
+      }
+      //TODO: add checks for who is alive
+      try{
+        Thread.sleep(sleep);
+      } catch (InterruptedException ex) {
+        if(isCancelled()) {
+          break;
+        }
+      }
+    }
+
+    public void addPlayer(User user, String colour, int direction){//TODO: change direction to enum
+      Player player = new Player(25, 25, colour, user, parent, direction);
+      playerList.add(player);
+    }
+
+    public boolean collides(int x, int y) {
+      return (outside(x,y)||map.get(getCellIndex(x,y)));
     }
   }
 }
