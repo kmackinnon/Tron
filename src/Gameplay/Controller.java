c5ffe1ce9bd6a30e5b935daf603665258a17fad0
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-26 16:48:51 -0500

    Added JavaDocs to Controller class

--- src/Gameplay/Controller.java
+++ src/Gameplay/Controller.java
@@ -14,29 +14,46 @@ public class Controller implements EventHandler<KeyEvent> {
      */
     static ConcurrentHashMap<KeyCode, Input> inputList = new ConcurrentHashMap<>();
 
-    private Controller() {
+    // to implement a singleton pattern
+    private Controller() {  
     }
 
+    /**
+     *
+     * @return the instance of Controller
+     */
     public static Controller getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * Resets the inputList.
+     */
     public void clear() {
         inputList.clear();
     }
 
+    /**
+     * Check the user-entered key and add it to the inputList
+     * @param input
+     */
     public void addBinding(Input input) {
         inputList.put(input.checkKey(), input);
     }
 
+    /**
+     * Tries to process the user-entered keys.
+     *
+     * @param e the key pressed
+     */
     @Override
     public void handle(KeyEvent e) {
         Input input = inputList.get(e.getCode());
-        
+
         // Need try-catch in case user types keys other than expected
         try {
-            input.command(); 
-        } catch (NullPointerException ex){
+            input.command();
+        } catch (NullPointerException ex) {
             // do nothing
         }
     }
