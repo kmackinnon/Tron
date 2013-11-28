3279fa104c2627fbb8e2dcd6d1a815cb28d8a5e0
Gabriel Petrella <petrella.gabriel@gmail.com>
2013-11-28 16:09:19 -0500

    Added gameplaytest package under tests, with class for controller testing. Fixed up javadocs for Player class

---
+++ test/gameplaytest/ControllerTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gameplaytest;
+
+import org.junit.*;
+import Gameplay.*;
+import UserInterface.*;
+
+/**
+ * Controller testing: don't need full in game setup, just check that key press functionality is good
+ * @author Gabriel
+ */
+public class ControllerTest {
+    
+    @Test
+    public void testPlayerOneMoveUp()
+    {
+        System.out.println("Test to see 'W' key.");
+    }
+    
+}
