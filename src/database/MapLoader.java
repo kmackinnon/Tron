9ddb36535c1999721c26c15b056218f9ecff87d0
Michael Williams <draringi@draringi.net>
2013-11-29 00:17:06 -0500

    Added Documentation for abstract MapLoader.

--- src/database/MapLoader.java
+++ src/database/MapLoader.java
@@ -9,10 +9,26 @@ import userinterface.Display;
  */
 public abstract class MapLoader {
 
+  /**
+   * Map name. Used for Identification in several storage backends.
+   * For files this would be the filename with the extension removed.
+   */
   protected String name;
     
+  /**
+   * Loads Map using Implementation specific backend.
+   * Returned Map is then ready to play on.
+   * @param display Display to show the map on.
+   * @param game Game this map is associated with.
+   * @return New Map to Play on.
+   */
   public abstract Map loadMap(Display display, GameInfo game);
 
+  /**
+   * Pulls the map Specs for the Map to load from, should it be in Binary format.
+   * Parsed String based formats will not need to use this function.
+   * @return Info on the map.
+   */
   protected abstract DatabaseInterface.MapSpecs openMap();
 
 }
