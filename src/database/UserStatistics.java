06d2d0c3ed64c4d3cdebcce1507d695f8727a732
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-28 22:31:50 -0500

    Javadocs to UserStatics as well as removing unused field myStats

--- src/database/UserStatistics.java
+++ src/database/UserStatistics.java
@@ -1,64 +1,64 @@
 package database;
 
+/**
+ * Contains user statistics necessary for tracking wins, losses, and games
+ * played.
+ *
+ * @author Michael Williams
+ */
 public class UserStatistics {
 
-  private int wins;
+    private int wins;
+    private int losses;
+    private int gamesPlayed;
 
-  private int gamesPlayed;
+    /**
+     * Increments number of wins and games played
+     */
+    public void addWin() {
+        wins++;
+        gamesPlayed++;
+    }
 
-  private int losses;
+    /**
+     * Increments number of losses and games played
+     */
+    public void addLoss() {
+        losses++;
+        gamesPlayed++;
+    }
 
-  private User myUser;
-  
-  /**
-   * Increments number of wins and games played
-   */
-  public void addWin(){
-    wins++;
-    gamesPlayed++;
-  }
-  
-  /**
-   * Increments number of losses and games played
-   */
-  public void addLoss(){
-    losses++;
-    gamesPlayed++;
-  }
+    /**
+     * @return the number of wins
+     */
+    public int getWins() {
+        return wins;
+    }
 
-  /**
-   * Retrieves number of wins
-   * @return 
-   */
-  public int getWins(){
-    return wins;
-  }
+    /**
+     * @return the number of losses
+     */
+    public int getLosses() {
+        return losses;
+    }
 
-  /**
-   * Retrieves number of losses
-   * @return 
-   */
-  public int getLosses(){
-    return losses;
-  }
+    /**
+     * @return the total number of games played
+     */
+    public int getGames() {
+        return gamesPlayed;
+    }
 
-  /**
-   * Retrieves total number of games played
-   * @return 
-   */
-  public int getGames(){
-    return gamesPlayed;
-  }
-
-  /**
-   * Creates new set of statistics for a User
-   * @param wins
-   * @param losses
-   * @param games 
-   */
-  public UserStatistics(int wins, int losses, int games){
-    this.wins = wins;
-    this.losses = losses;
-    this.gamesPlayed = games;
-  }
+    /**
+     * Passes in new statistics for a User.
+     *
+     * @param wins
+     * @param losses
+     * @param games
+     */
+    public UserStatistics(int wins, int losses, int games) {
+        this.wins = wins;
+        this.losses = losses;
+        this.gamesPlayed = games;
+    }
 }
