09dcc168d1012fb97ad8fba997d298f87cfc9338
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-29 04:19:20 -0500

    Currently getting 81% passing rate. Cannot figure out the remaining tests. There seems to be some problem any time I call something on db.

--- test/database/UserTest.java
+++ test/database/UserTest.java
@@ -1,5 +1,11 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -8,41 +14,226 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Test all database queries. This relates to login and statistics.
+ * Test all methods of User. Ensure to delete existing databases!
  *
  * @author Keith MacKinnon
  */
 public class UserTest {
     
-    User testUser;
+    private static User testUser;
+    private static DatabaseInterface db;
 
     public UserTest() {
     }
 
+    /**
+     * Create a new database and a testUser before running the tests are executed.
+     */
     @BeforeClass
     public static void setUpClass() {
+        db = new SQLiteInterface(".testing.db");
     }
 
     @AfterClass
     public static void tearDownClass() {
+
     }
 
     @Before
     public void setUp() {
-        testUser = new User(1, "Keith"); //created a random User with uid=1
+        testUser = new User(1, "Sankalp"); // creates a user with uid = 1
     }
 
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    
+    /**
+     * Test of createUser method, of class User.
+     */
+    @Test
+    public void testCreateUser() {
+        System.out.println("createUser");
+        String username = "Nancy";
+        String password = "Nancy93!";
+        User expResult = db.addUser(username, password);
+        User result = User.createUser(username, password);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of changePassword method, of class User.
+     */
+    @Test
+    public void testChangePassword() {
+        System.out.println("changePassword");
+        String password = "testUser93!";
+        User instance = testUser;
+        instance.changePassword(password);
+    }
+
+    /**
+     * Test of login method, of class User.
+     */
+    @Test
+    public void testLogin() {
+        System.out.println("login");
+        String password = "testUser93!";
+        User instance = testUser;
+        instance.login(password);
+    }
+
+    /**
+     * Test of winGame method, of class User.
+     */
+    @Test
+    public void testWinGame() {
+        System.out.println("winGame");
+        User instance = testUser;
+        instance.winGame();
+    }
+
+    /**
+     * Test of loseGame method, of class User.
+     */
+    @Test
+    public void testLoseGame() {
+        System.out.println("loseGame");
+        User instance = testUser;
+        instance.loseGame();
+    }
+
+    /**
+     * Test of getID method, of class User.
+     */
+    @Test
+    public void testGetID() {
+        System.out.println("getID");
+        User instance = testUser;
+        int expResult = 1;
+        int result = instance.getID();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getStats method, of class User.
+     */
+    @Test
+    public void testGetStats() {
+        System.out.println("getStats");
+        User instance = testUser;
+        UserStatistics expResult = db.getUserStats(1);
+        UserStatistics result = instance.getStats();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of isAuthenticated method, of class User.
+     */
+    @Test
+    public void testIsAuthenticated() {
+        System.out.println("isAuthenticated");
+        User instance = testUser;
+        boolean expResult = false; // haven't actually authenticated user
+        boolean result = instance.isAuthenticated();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getTopTen method, of class User.
+     */
+    @Test
+    public void testGetTopTen() {
+        System.out.println("getTopTen");
+        String[][] expResult = {{"Sankalp","0"},
+            {"Nancy","0"}};               
+        String[][] result = User.getTopTen();
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of getGamesWon method, of class User.
+     */
+    @Test
+    public void testGetGamesWon() {
+        System.out.println("getGamesWon");
+        User instance = testUser;
+        int expResult = 0; //since user gets overwritten after each method
+        int result = instance.getGamesWon();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getGamesPlayed method, of class User.
+     */
+    @Test
+    public void testGetGamesPlayed() {
+        System.out.println("getGamesPlayed");
+        User instance = testUser;
+        int expResult = 0;
+        int result = instance.getGamesPlayed();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getGamesLost method, of class User.
+     */
+    @Test
+    public void testGetGamesLost() {
+        System.out.println("getGamesLost");
+        User instance = testUser;
+        int expResult = 0;
+        int result = instance.getGamesLost();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of saveStats method, of class User.
+     */
+    @Test
+    public void testSaveStats() {
+        System.out.println("saveStats");
+        User instance = testUser;
+        instance.saveStats();
+    }
+
+    /**
+     * Test of getHead2HeadStats method, of class User.
+     */
+    @Test
+    public void testGetHead2HeadStats() {
+        System.out.println("getHead2HeadStats");
+        User user1 = testUser;
+        User user2 = db.getUser("Nancy");
+        int[] expResult = {0,0};
+        int[] result = User.getHead2HeadStats(user1, user2);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of getUser method, of class User.
+     */
+    @Test
+    public void testGetUser() {
+        System.out.println("getUser");
+        String username = "Nancy";
+        String password = "Nancy93!";
+        User expResult = db.getUser(username);
+        User result = User.getUser(username, password);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getUsername method, of class User.
+     */
     @Test
     public void testGetUsername() {
-        assertTrue(testUser.getUsername().equals("Keith")); 
-        assertFalse(testUser.getUsername().equals("eith")); 
+        System.out.println("getUsername");
+        User instance = testUser;
+        String expResult = "Sankalp";
+        String result = instance.getUsername();
+        assertEquals(expResult, result);
+        assertNotEquals("Keith", result);
     }
    
 }
